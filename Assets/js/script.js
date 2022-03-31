31161ce5dd45bd7c17f6e63f57c80f210b704a79
Carolyn Ngo <carolynngo11@yahoo.com>
2022-03-31 01:24:49 -0500

    Added margin to the bottom

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,9 @@
 // Time function (ref: moment.js)
 var current = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").html(current);
+
+// Save User's Text Input Function
+
+// Live Event Time Tracker Function
+
+// 
